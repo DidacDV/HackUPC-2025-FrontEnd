8b5b4ff7d880e0ef828624e60fc4a71078f50846
keigratz <113896749+DidacDV@users.noreply.github.com>
2025-05-03 16:55:15 +0200

    Added common colors from Revolut palette

---
+++ lib/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lime = Color(0xFFBFFF37);
+  static const Color purple = Color(0xFF9539F2);
+  static const Color lightPurple = Color(0xFFCACCFB);
+  static const Color lightBlue = Color(0xFFC6D9FD);
+  static const Color midBlue = Color(0xFF6FA0FF);
+  static const Color deepBlue = Color(0xFF1326FD);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+}
